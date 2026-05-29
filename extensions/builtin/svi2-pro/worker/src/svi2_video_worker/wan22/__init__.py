365c23d67ec9d9941366ca53e937d45dbd91ea21
Lazar Dilov <ldilov@yahoo.com>
2026-05-29 23:08:08 +0300

    feat(svi2-pro): vendor WanTextEncoder (UMT5) + wire TextEncoderWrapper

--- extensions/builtin/svi2-pro/worker/src/svi2_video_worker/wan22/__init__.py
+++ extensions/builtin/svi2-pro/worker/src/svi2_video_worker/wan22/__init__.py
@@ -1,5 +1,13 @@
 from .dit import WanModel, sinusoidal_embedding_1d
 from .flow_match import FlowMatchScheduler
 from .vae_model import WanVideoVAE
+from .text_encoder_model import WanTextEncoder, HuggingfaceTokenizer
 
-__all__ = ["WanModel", "FlowMatchScheduler", "sinusoidal_embedding_1d", "WanVideoVAE"]
+__all__ = [
+    "WanModel",
+    "FlowMatchScheduler",
+    "sinusoidal_embedding_1d",
+    "WanVideoVAE",
+    "WanTextEncoder",
+    "HuggingfaceTokenizer",
+]
