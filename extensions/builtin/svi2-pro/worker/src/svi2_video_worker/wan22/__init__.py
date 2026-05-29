a3b945bd9beb55c81dc84636a056ae346b1f6c6b
Lazar Dilov <ldilov@yahoo.com>
2026-05-29 23:00:07 +0300

    feat(svi2-pro): vendor WanVideoVAE + wire VaeWrapper encode/decode

--- extensions/builtin/svi2-pro/worker/src/svi2_video_worker/wan22/__init__.py
+++ extensions/builtin/svi2-pro/worker/src/svi2_video_worker/wan22/__init__.py
@@ -1,4 +1,5 @@
 from .dit import WanModel, sinusoidal_embedding_1d
 from .flow_match import FlowMatchScheduler
+from .vae_model import WanVideoVAE
 
-__all__ = ["WanModel", "FlowMatchScheduler", "sinusoidal_embedding_1d"]
+__all__ = ["WanModel", "FlowMatchScheduler", "sinusoidal_embedding_1d", "WanVideoVAE"]
